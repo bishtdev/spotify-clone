6347dcf612eb2f0d8dc6ce5fb94e5bcdb4f06059
bishtdev <bishtdev030@gmail.com>
2024-05-31 16:34:13 +0530

    setting up the router

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import { albumsData, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
@@ -24,8 +25,14 @@ const DisplayHome = () => {
       <div className=" mb-4">
         <h1 className="my-5 font-bold text-2xl"> Today's biggest hit</h1>
         <div className="flex overflow-auto">
-          {songsData.map((item , index )=>(
-            
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
           ))}
         </div>
       </div>
