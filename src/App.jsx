f17802b2341c44186e49c733aecbbcebcbf59b74
bishtdev <bishtdev030@gmail.com>
2024-06-01 15:55:09 +0530

    added the player context for player functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 function App() {
+
+  const {audioRef, track} = useContext(PlayerContext)
+ 
 
   return (
    <div className='h-screen bg-black'>
@@ -13,6 +17,7 @@ function App() {
       <Display/>
     </div>
     <Player/>
+    <audio ref={audioRef} src={track.file} preload='auto'></audio>
    </div>
   )
 }
---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+    const audioRef = useRef();
+    const seekBg = useRef()
+    const seekBar = useRef()
+
+    const [track, setTrack] = useState(songsData[0])
+    const [playStatus, setPlayStatus] = useState(false)
+    const [time, setTime] = useState({
+        currentTime: {
+            secound: 0,
+            minute: 0
+        },
+        totalTime: {
+            secound: 0,
+            minute: 0
+        }
+    })
+
+    const play = () =>{
+        audioRef.current.play();
+        setPlayStatus(true)
+    }
+
+    const pause = () =>{
+        audioRef.current.pause()
+        setPlayStatus(false)
+    }
+
+    const contextValue = { audioRef, seekBg, seekBar, track, setTrack, playStatus, setPlayStatus, time, setTime, play, pause };
+
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.children}
+        </PlayerContext.Provider>
+    );
+};
+
+export default PlayerContextProvider;
