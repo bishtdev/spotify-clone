51acc7d01c8e14b5465f80db89224d62baecdaa9
bishtdev <bishtdev030@gmail.com>
2024-06-01 16:43:53 +0530

    added the playbar functionality

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,37 +1,67 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
     const audioRef = useRef();
-    const seekBg = useRef()
-    const seekBar = useRef()
+    const seekBg = useRef();
+    const seekBar = useRef();
 
-    const [track, setTrack] = useState(songsData[0])
-    const [playStatus, setPlayStatus] = useState(false)
+    const [track, setTrack] = useState(songsData[3]);
+    const [playStatus, setPlayStatus] = useState(false);
     const [time, setTime] = useState({
         currentTime: {
-            secound: 0,
-            minute: 0
+            second: 0,
+            minute: 0,
         },
         totalTime: {
-            secound: 0,
-            minute: 0
-        }
-    })
+            second: 0,
+            minute: 0,
+        },
+    });
 
-    const play = () =>{
+    const play = () => {
         audioRef.current.play();
-        setPlayStatus(true)
-    }
+        setPlayStatus(true);
+    };
 
-    const pause = () =>{
-        audioRef.current.pause()
-        setPlayStatus(false)
-    }
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false);
+    };
 
-    const contextValue = { audioRef, seekBg, seekBar, track, setTrack, playStatus, setPlayStatus, time, setTime, play, pause };
+    useEffect(() => {
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+'%'
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60),
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60),
+                    }
+                })
+            }
+        }, 1000);
+    }, [audioRef]);
+
+    const contextValue = {
+        audioRef,
+        seekBg,
+        seekBar,
+        track,
+        setTrack,
+        playStatus,
+        setPlayStatus,
+        time,
+        setTime,
+        play,
+        pause,
+    };
 
     return (
         <PlayerContext.Provider value={contextValue}>
